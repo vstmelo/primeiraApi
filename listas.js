b68cf01a3a6a36599001f67242b909060b919894
Vinicius <vinicius.melo@dcomp.ufs.br>
2021-06-29 01:49:39 -0300

    feature: added lists.

---
+++ listas.js
@@ -0,0 +1,12 @@
+let lista = {
+    "id": "lista-0",
+    "nome": "Lista",
+    "tarefas": [],
+    "mumeroDeTarefas": 0
+}
+
+let tarefa = {
+    "id": "tarefa-0",
+    "descricao": "Testar conhecimentos !",
+    "completar": true
+}
