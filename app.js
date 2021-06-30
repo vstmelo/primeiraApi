2846dbf833bc33f20b1eb6834b7d9392c964ad44
Vinicius <vinicius.melo@dcomp.ufs.br>
2021-06-30 13:56:15 -0300

    feature: add routes

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
-var express = require("express"); // import express package.
+import express from "express"; // import express package.
 var app = express (); // initialized with express.
+var bodyParser = requiere("body-parser");
+//importando modulo
+import listaDAO from "./listaDAO";
+//iniciando express
+var app = express();
+//configurar metpdp para ler 'body' requests
+app.use(bodyParser.json());
+// criar rotas aqui
+app.get("/teste", function(requeste, response){
+    console.log("Rota /teste acessada com sucesso!");
+});
+
+app.listen(8080);
